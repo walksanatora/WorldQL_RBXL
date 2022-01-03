d153269ac322044d9a991dceb2c66904f2a70f46
walksanatora <walkerffo22@hotmail.com>
2022-01-02 22:13:46 -0800

    time to test it and see if it conencts

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,5 @@
 print('init-server')
+local RepStor = game:GetService('ReplicatedStorage')
+local WQL = require(RepStor.common.WorldQL)
+
+local client = WQL.createNew('furry-act.auto.playit.gg:41075',2)
