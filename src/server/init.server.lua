74367e3b1f923ed80fb704b8e047edd1ad179000
walksanatora <walkerffo22@hotmail.com>
2022-01-03 15:20:10 -0800

    globalised chat test time

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,4 +1,5 @@
 print('init-server')
+local chat = game:GetService('Chat')
 local RepStor = game:GetService('ReplicatedStorage')
 local WQL = require(RepStor.Common.WorldQL)
 local WQL_Types = require(RepStor.Common.WorldQL.DataTypes)
@@ -6,23 +7,18 @@ local WQL_Types = require(RepStor.Common.WorldQL.DataTypes)
 local client = WQL.createNew('http://furry-act.auto.playit.gg:41075',2)
 client.on('ready',function()
     print('the client has successfully connected')
+    chat:Connect(function(part,message,color)
+        if color == Enum.ChatColor.Green then
+            return
+        else
+            client.sendGlobalMessage('roblox',WQL_Types.Enum.Replication.ExceptSelf,{
+                ['parameter'] = message,
+                ['flex'] = 'rblxChat',
+            })
+        end
+    end)
 end)
 client.on('disconnect',function()
     print('the client disconnected boo-hoo')
 end)
 client.connect()
-wait(5)
-for i = 0, 10, 1 do
-    client.sendRawMessage({
-        ['instruction'] = WQL_Types.Enum.Instruction.GlobalMessage,
-        ['parameter'] = 'Hello From Roblox '..tostring(i)..'/10',
-        ['worldName'] = 'ROBLOX',
-        ['replication'] = WQL_Types.Enum.Replication.ExceptSelf,
-        ['position'] = {
-            ['x'] = i,
-            ['y'] = i*5,
-            ['z'] = i^i
-        }
-    })
-    wait(10)
-end
