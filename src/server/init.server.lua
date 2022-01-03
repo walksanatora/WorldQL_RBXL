e38cb7d79cc06d7296d8525b8af25693f5c5d6f8
walksanatora <walkerffo22@hotmail.com>
2022-01-03 15:30:07 -0800

    global message handler

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,5 +1,8 @@
 print('init-server')
 local chat = game:GetService('Chat')
+local i = Instance.new('Part')
+i.Parent = workspace
+i.Name = 'WQL_Chat'
 local RepStor = game:GetService('ReplicatedStorage')
 local WQL = require(RepStor.Common.WorldQL)
 local WQL_Types = require(RepStor.Common.WorldQL.DataTypes)
@@ -7,7 +10,7 @@ local WQL_Types = require(RepStor.Common.WorldQL.DataTypes)
 local client = WQL.createNew('http://furry-act.auto.playit.gg:41075',2)
 client.on('ready',function()
     print('the client has successfully connected')
-    chat:Connect(function(part,message,color)
+    chat.Chatted:Connect(function(part,message,color)
         if color == Enum.ChatColor.Green then
             return
         else
@@ -21,5 +24,10 @@ client.on('ready',function()
 end)
 client.on('disconnect',function()
     print('the client disconnected boo-hoo')
+end)
+client.on('globalMessage',function(message)
+    if message.flex == 'rbxlChat' then
+        chat:Chat(i,message.parameter,Enum.ChatColor.Green)
+    end
 end)
 client.connect()
